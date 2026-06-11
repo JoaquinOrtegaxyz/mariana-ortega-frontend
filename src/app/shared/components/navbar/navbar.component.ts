ffe42f089bcee2f5436ee277984d673676f8fab6
Joaquin <joaco.rodriguezortega@gmail.com>
2026-06-11 03:38:46 -0300

    login, wpp fb ig icons, more routes and page logo

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // <--- 1. IMPORTÁ ESTO
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule // <--- 2. AGREGALO ACÁ EN LOS IMPORTS
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  // Tu lógica del navbar queda igual
 }
