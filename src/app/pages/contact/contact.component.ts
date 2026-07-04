66696042e55570c8ed47213ecb3ffb52e73f7a6f
Joaquin <joaco.rodriguezortega@gmail.com>
2026-07-04 19:08:20 -0300

    last changes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-component',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const form = this.contactForm.value;
+
+      const texto = `Hola Mariana, soy ${form.name}.%0A%0A${form.message}%0A%0A, Mis datos de contacto:%0ATel: ${form.phone}%0AEmail: ${form.email}`;
+      const whatsappUrl = `https://wa.me/5492262579622?text=${texto}`;
+
+      window.open(whatsappUrl, '_blank');
+      this.contactForm.reset();
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
--- src/app/pages/property-detail/property-detail.component.ts
+++ src/app/pages/property-detail/property-detail.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { PropertyService } from '../../services/property.service';
 
 @Component({
   selector: 'app-property-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './property-detail.component.html'
 })
 export class PropertyDetailComponent implements OnInit {
@@ -19,23 +18,13 @@ export class PropertyDetailComponent implements OnInit {
 
   images: string[] = [];
   currentImageIndex: number = 0;
-
-  contactForm: FormGroup;
-
   mapUrl: SafeResourceUrl | null = null;
 
   constructor(
     private route: ActivatedRoute,
     private propertyService: PropertyService,
-    private fb: FormBuilder,
     private sanitizer: DomSanitizer
-  ) {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      message: ['Hola, me interesa esta propiedad y quiero más información.', Validators.required]
-    });
-  }
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -55,14 +44,12 @@ export class PropertyDetailComponent implements OnInit {
       next: (data) => {
         this.property = data;
 
-        // MAGIA DEL MAPA: Le tiramos todos los parámetros a Google para forzar el pin (iwloc=B)
         if (this.property.location?.street) {
           const address = `${this.property.location.street} ${this.property.location.streetNumber || ''}, Necochea, Buenos Aires, Argentina`;
           const url = `https://maps.google.com/maps?width=100%25&height=100%25&hl=es-419&q=${encodeURIComponent(address)}&t=&z=15&ie=UTF8&iwloc=B&output=embed`;
           this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
         }
 
-        // Lógica de imágenes
         if (this.property.images && this.property.images.length > 0) {
           const coverImg = this.property.images.find((i: any) => i.isCover) || this.property.images[0];
           const otherImgs = this.property.images.filter((i: any) => i.id !== coverImg.id);
@@ -82,32 +69,14 @@ export class PropertyDetailComponent implements OnInit {
   }
 
   nextImage() {
-    if (this.currentImageIndex < this.images.length - 1) {
-      this.currentImageIndex++;
-    } else {
-      this.currentImageIndex = 0;
-    }
+    this.currentImageIndex = (this.currentImageIndex < this.images.length - 1) ? this.currentImageIndex + 1 : 0;
   }
 
   prevImage() {
-    if (this.currentImageIndex > 0) {
-      this.currentImageIndex--;
-    } else {
-      this.currentImageIndex = this.images.length - 1;
-    }
+    this.currentImageIndex = (this.currentImageIndex > 0) ? this.currentImageIndex - 1 : this.images.length - 1;
   }
 
   setMainImage(index: number) {
     this.currentImageIndex = index;
-  }
-
-  submitContact() {
-    if (this.contactForm.valid) {
-      console.log('Mensaje para enviar:', this.contactForm.value);
-      alert('¡Mensaje enviado con éxito! La inmobiliaria se contactará a la brevedad.');
-      this.contactForm.reset();
-    } else {
-      this.contactForm.markAllAsTouched();
-    }
   }
 }
