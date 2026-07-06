41a0b1f0d8f3049fe8ad2f9633ec2625450157c7
Joaquin <joaco.rodriguezortega@gmail.com>
2026-07-06 05:42:05 -0300

    .

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inmobiliaria-necochea.vercel.app/api'
+  apiUrl: 'https://mariana-ortega-backend-production.up.railway.app/api'
 };
